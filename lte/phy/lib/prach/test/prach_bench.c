696eed6d147e85cb5c447efa89a8f8a88d83192f
Paul Sutton <suttonpd@gmail.com>
2014-06-24 17:41:08 +0100

    Adding prach for uplink - supports preamble formats 0-3, does not currently support high-speed flag, benchmark not yet implemented

---
+++ lte/phy/lib/prach/test/prach_bench.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+
+#include "liblte/phy/prach/prach.h"
+
+typedef _Complex float cf_t;
+
+int main(int argc, char **argv) {
+
+	exit(0);
+}
