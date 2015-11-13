1ea2f379a7fe4a8f737a5ebcd28e09c7f89626ed
Paul Sutton <suttonpd@gmail.com>
2015-11-13 12:22:33 +0000

    Updating copyright notices

--- srslte/lib/common/src/sequence.c
+++ srslte/lib/common/src/sequence.c
@@ -2,8 +2,7 @@
  *
  * \section COPYRIGHT
  *
- * Copyright 2013-2015 The srsLTE Developers. See the
- * COPYRIGHT file at the top-level directory of this distribution.
+ * Copyright 2013-2015 Software Radio Systems Limited
  *
  * \section LICENSE
  *
