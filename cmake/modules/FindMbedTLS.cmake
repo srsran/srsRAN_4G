ad48f0d347c5a8aadcec71410dbe6b063c46c2b3
Andre Puschmann <andre@softwareradiosystems.com>
2017-05-18 12:55:13 +0200

    add cmake find scripts for polarssl and mbedtls

---
+++ cmake/modules/FindMbedTLS.cmake
@@ -0,0 +1,40 @@
+# - Try to find mbedtls
+#
+# Once done this will define
+#  MBEDTLS_FOUND        - System has mbedtls
+#  MBEDTLS_INCLUDE_DIRS - The mbedtls include directories
+#  MBEDTLS_LIBRARIES    - The mbedtls library
+
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_MBEDTLS mbedtls)
+
+#find Mbedtls
+FIND_PATH(
+    MBEDTLS_INCLUDE_DIRS
+    NAMES mbedtls/md.h
+    HINTS $ENV{MBEDTLS_DIR}/include
+          ${PC_MBEDTLS_INCLUDEDIR}
+          ${CMAKE_INSTALL_PREFIX}/include
+    PATHS /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    MBEDTLS_LIBRARIES
+    NAMES mbedcrypto
+    HINTS $ENV{MBEDTLS_DIR}/lib
+          ${PC_MBEDTLS_LIBDIR}
+          ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+    PATHS /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+message(STATUS "MBEDTLS LIBRARIES: " ${MBEDTLS_LIBRARIES})
+message(STATUS "MBEDTLS INCLUDE DIRS: " ${MBEDTLS_INCLUDE_DIRS})
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MBEDTLS DEFAULT_MSG MBEDTLS_LIBRARIES MBEDTLS_INCLUDE_DIRS)
+MARK_AS_ADVANCED(MBEDTLS_LIBRARIES MBEDTLS_INCLUDE_DIRS)
---
+++ cmake/modules/FindPolarssl.cmake
@@ -0,0 +1,39 @@
+# - Try to find polarssl
+#
+# Once done this will define
+#  POLARSSL_FOUND        - System has polarssl
+#  POLARSSL_INCLUDE_DIRS - The polarssl include directories
+#  POLARSSL_LIBRARIES    - The polarssl library
+
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_POLARSSL polarssl)
+
+FIND_PATH(
+    POLARSSL_INCLUDE_DIRS
+    NAMES polarssl/version.h
+    HINTS $ENV{POLARSSL_DIR}/include
+          ${PC_POLARSSL_INCLUDEDIR}
+          ${CMAKE_INSTALL_PREFIX}/include
+    PATHS /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    POLARSSL_LIBRARIES
+    NAMES polarssl
+    HINTS $ENV{POLARSSL_DIR}/lib
+          ${PC_POLARSSL_LIBDIR}
+          ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+    PATHS /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+message(STATUS "POLARSSL LIBRARIES: " ${POLARSSL_LIBRARIES})
+message(STATUS "POLARSSL INCLUDE DIRS: " ${POLARSSL_INCLUDE_DIRS})
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(POLARSSL DEFAULT_MSG POLARSSL_LIBRARIES POLARSSL_INCLUDE_DIRS)
+MARK_AS_ADVANCED(POLARSSL_LIBRARIES POLARSSL_INCLUDE_DIRS)
