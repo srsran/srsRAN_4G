1d095b006adc7db940f9b7d7ebc6672f352db64a
Ismael Gomez <ismagom@gmail.com>
2017-10-28 23:30:29 -0700

    Removed unused nof_tb variable

--- lib/include/srslte/phy/phch/ra.h
+++ lib/include/srslte/phy/phch/ra.h
@@ -104,7 +104,6 @@ typedef struct SRSLTE_API {
   uint32_t nof_prb;  
   uint32_t Qm[SRSLTE_MAX_CODEWORDS];
   srslte_ra_mcs_t mcs[SRSLTE_MAX_CODEWORDS];
-  uint32_t nof_tb;
   srslte_sf_t sf_type;
   bool tb_en[SRSLTE_MAX_CODEWORDS];
   uint32_t pinfo;
