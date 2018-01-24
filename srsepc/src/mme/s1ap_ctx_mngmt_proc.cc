aa437806d8218b03d4032487e61304afac327bad
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2018-01-24 15:47:06 +0000

    rm boost::mutex from s1ap_ctx_mngmt

--- srsepc/src/mme/s1ap_ctx_mngmt_proc.cc
+++ srsepc/src/mme/s1ap_ctx_mngmt_proc.cc
@@ -34,7 +34,7 @@
 namespace srsepc{
 
 s1ap_ctx_mngmt_proc*          s1ap_ctx_mngmt_proc::m_instance = NULL;
-boost::mutex   s1ap_ctx_mngmt_proc_instance_mutex;
+pthread_mutex_t s1ap_ctx_mngmt_proc_instance_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 s1ap_ctx_mngmt_proc::s1ap_ctx_mngmt_proc()
@@ -48,21 +48,23 @@ s1ap_ctx_mngmt_proc::~s1ap_ctx_mngmt_proc()
 s1ap_ctx_mngmt_proc*
 s1ap_ctx_mngmt_proc::get_instance(void)
 {
-  boost::mutex::scoped_lock lock(s1ap_ctx_mngmt_proc_instance_mutex);
+  pthread_mutex_lock(&s1ap_ctx_mngmt_proc_instance_mutex);
   if(NULL == m_instance) {
     m_instance = new s1ap_ctx_mngmt_proc();
   }
+  pthread_mutex_unlock(&s1ap_ctx_mngmt_proc_instance_mutex);
   return(m_instance);
 }
 
 void
 s1ap_ctx_mngmt_proc::cleanup(void)
 {
-  boost::mutex::scoped_lock lock(s1ap_ctx_mngmt_proc_instance_mutex);
+  pthread_mutex_lock(&s1ap_ctx_mngmt_proc_instance_mutex);
   if(NULL != m_instance) {
     delete m_instance;
     m_instance = NULL;
   }
+  pthread_mutex_unlock(&s1ap_ctx_mngmt_proc_instance_mutex);
 }
 
 void
