296d49f3eb20d98592318f0bf3542182497997da
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2018-04-10 19:45:06 +0200

    Now enb does not crash when the QCI is not invalid.

--- srsenb/src/upper/rrc.cc
+++ srsenb/src/upper/rrc.cc
@@ -1447,7 +1447,8 @@ void rrc::ue::send_connection_reconf(srslte::byte_buffer_t *pdu)
   // Get DRB1 configuration 
   if (get_drbid_config(&conn_reconf->rr_cnfg_ded.drb_to_add_mod_list[0], 1)) {
     parent->rrc_log->error("Getting DRB1 configuration\n");
-    printf("ERROR: The QCI %d is invalid or not configured.\n", erabs[5].qos_params.qCI.QCI);
+    printf("The QCI %d for DRB1 is invalid or not configured.\n", erabs[5].qos_params.qCI.QCI);
+    return;
   } else {
     conn_reconf->rr_cnfg_ded.drb_to_add_mod_list_size = 1; 
   }
@@ -1528,6 +1529,7 @@ void rrc::ue::send_connection_reconf_new_bearer(LIBLTE_S1AP_E_RABTOBESETUPLISTBE
     if (get_drbid_config(&conn_reconf->rr_cnfg_ded.drb_to_add_mod_list[i], lcid)) {
       parent->rrc_log->error("Getting DRB configuration\n");    
       printf("ERROR: The QCI %d is invalid or not configured.\n", erabs[lcid+4].qos_params.qCI.QCI);
+      return;
     } else {
       conn_reconf->rr_cnfg_ded.drb_to_add_mod_list_size++;
     }
