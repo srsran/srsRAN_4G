3631c4a83f5fb72daaad69c0b6ab067692235c13
Ismael Gomez <ismael@softwareradiosystems.com>
2018-06-04 16:41:39 +0200

    Changed ue.conf op to opc

--- srsue/src/main.cc
+++ srsue/src/main.cc
@@ -129,7 +129,7 @@ void parse_args(all_args_t *args, int argc, char *argv[]) {
 
     ("usim.mode", bpo::value<string>(&args->usim.mode)->default_value("soft"), "USIM mode (soft or pcsc)")
     ("usim.algo", bpo::value<string>(&args->usim.algo), "USIM authentication algorithm")
-    ("usim.op", bpo::value<string>(&args->usim.op), "USIM operator variant")
+    ("usim.opc", bpo::value<string>(&args->usim.op), "USIM operator ciphered variant")
     ("usim.imsi", bpo::value<string>(&args->usim.imsi), "USIM IMSI")
     ("usim.imei", bpo::value<string>(&args->usim.imei), "USIM IMEI")
     ("usim.k", bpo::value<string>(&args->usim.k), "USIM K")
