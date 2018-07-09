ff49e568717ec4e27bdea57d9c3d262c2966bcdc
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2018-07-09 18:25:43 +0100

    Starting to add s1ap_pcap class.

--- lib/src/common/s1ap_pcap.cc
+++ lib/src/common/s1ap_pcap.cc
@@ -1,33 +1,32 @@
 #include <stdint.h>
 #include "srslte/srslte.h"
 #include "srslte/common/pcap.h"
-#include "srslte/common/nas_pcap.h"
+#include "srslte/common/s1ap_pcap.h"
 
 
 namespace srslte {
 
-void nas_pcap::enable()
+void s1ap_pcap::enable()
 {
   enable_write = true;
 }
-void nas_pcap::open(const char* filename, uint32_t ue_id)
+void s1ap_pcap::open(const char* filename)
 {
-  pcap_file = LTE_PCAP_Open(NAS_LTE_DLT, filename);
-  ue_id = ue_id;
+  pcap_file = LTE_PCAP_Open(S1AP_LTE_DLT, filename);
   enable_write = true;
 }
-void nas_pcap::close()
+void s1ap_pcap::close()
 {
-  fprintf(stdout, "Saving NAS PCAP file\n");
+  fprintf(stdout, "Saving S1AP PCAP file\n");
   LTE_PCAP_Close(pcap_file);
 }
 
-void nas_pcap::write_nas(uint8_t *pdu, uint32_t pdu_len_bytes)
+void s1ap_pcap::write_s1ap(uint8_t *pdu, uint32_t pdu_len_bytes)
 {
     if (enable_write) {
-      NAS_Context_Info_t  context;
+      S1AP_Context_Info_t  context;
       if (pdu) {
-        LTE_PCAP_NAS_WritePDU(pcap_file, &context, pdu, pdu_len_bytes);
+        LTE_PCAP_S1AP_WritePDU(pcap_file, &context, pdu, pdu_len_bytes);
       }
     }
 }
