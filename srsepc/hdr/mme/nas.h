41a5788cc7fa5644470af11981b6cc33b443d26b
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2018-07-13 15:27:38 +0100

    Adding nas.h

---
+++ srsepc/hdr/mme/nas.h
@@ -0,0 +1,202 @@
+/*
+ * \section LICENSE
+ *
+ * This file is part of srsLTE.
+ *
+ * srsLTE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * srsLTE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * A copy of the GNU Affero General Public License can be found in
+ * the LICENSE file in the top-level directory of this distribution
+ * and at http://www.gnu.org/licenses/.
+ *
+ */
+#ifndef SRSEPC_S1AP_COMMON_H
+#define SRSEPC_S1AP_COMMON_H
+
+#include "srslte/common/security.h"
+#include "srslte/asn1/gtpc_ies.h"
+#include "srslte/asn1/liblte_s1ap.h"
+#include "srslte/asn1/liblte_mme.h"
+#include <netinet/sctp.h>
+
+namespace srsepc{
+
+static const uint8_t MAX_ERABS_PER_UE = 16;
+
+// MME EMM states (3GPP 24.301 v10.0.0, section 5.1.3.4)
+typedef enum {
+  EMM_STATE_DEREGISTERED = 0,
+  EMM_STATE_COMMON_PROCEDURE_INITIATED,
+  EMM_STATE_REGISTERED,
+  EMM_STATE_DEREGISTERED_INITIATED,
+  EMM_STATE_N_ITEMS,
+} emm_state_t;
+static const char emm_state_text[EMM_STATE_N_ITEMS][100] = {"DEREGISTERED",
+                                                            "COMMON PROCEDURE INITIATED",
+                                                            "REGISTERED",
+                                                            "DEREGISTERED INITIATED"};
+
+// MME ECM states (3GPP 23.401 v10.0.0, section 4.6.3)
+typedef enum {
+  ECM_STATE_IDLE = 0,
+  ECM_STATE_CONNECTED,
+  ECM_STATE_N_ITEMS,
+} ecm_state_t;
+static const char ecm_state_text[ECM_STATE_N_ITEMS][100] = {"IDLE",
+                                                            "CONNECTED"};
+
+// MME ESM states (3GPP 23.401 v10.0.0, section 4.6.3)
+typedef enum {
+  ESM_BEARER_CONTEXT_INACTIVE = 0,
+  ESM_BEARER_CONTEXT_ACTIVE_PENDING,
+  ESM_BEARER_CONTEXT_ACTIVE,
+  ESM_BEARER_CONTEXT_INACTIVE_PENDING,
+  ESM_BEARER_CONTEXT_MODIFY_PENDING,
+  ESM_BEARER_PROCEDURE_TRANSACTION_INACTIVE,
+  ESM_BEARER_PROCEDURE_TRANSACTION_PENDING,
+  ESM_STATE_N_ITEMS,
+} esm_state_t;
+static const char esm_state_text[ESM_STATE_N_ITEMS][100] = {"CONTEXT INACTIVE",
+                                                            "CONTEXT ACTIVE PENDING",
+                                                            "CONTEXT ACTIVE",
+                                                            "CONTEXT_INACTIVE_PENDING",
+                                                            "CONTEXT_MODIFY_PENDING",
+                                                            "PROCEDURE_TRANSACTION_INACTIVE"
+                                                            "PROCEDURE_TRANSACTION_PENDING"};
+
+/*
+ * EMM, ECM, ESM and EPS Security context definition
+ */
+typedef struct{
+    uint64_t imsi;
+    LIBLTE_MME_EPS_MOBILE_ID_GUTI_STRUCT guti;
+    eps_sec_ctx_t security_ctxt;
+    uint8_t procedure_transaction_id;
+    emm_state_t state;
+    uint32_t mme_ue_s1ap_id;
+    uint8_t attach_type;
+    struct in_addr ue_ip;
+    srslte::gtpc_f_teid_ie sgw_ctrl_fteid;
+} emm_ctx_t;
+
+typedef struct{
+  uint64_t imsi;
+  uint32_t enb_ue_s1ap_id;
+  uint32_t mme_ue_s1ap_id;
+  struct   sctp_sndrcvinfo enb_sri;
+  ecm_state_t state;
+  bool eit;
+} ecm_ctx_t;
+
+typedef struct{
+  enum erab_state state;
+  uint8_t erab_id;
+  uint8_t qci;
+  srslte::gtpc_f_teid_ie enb_fteid;
+  srslte::gtpc_f_teid_ie sgw_s1u_fteid;
+  srslte::gtpc_pdn_address_allocation_ie pdn_addr_alloc;
+} erab_ctx_t;
+
+typedef struct{
+  uint8_t  eksi;
+  uint8_t  k_asme[32];
+  uint8_t  xres[16]; //minimum 6, maximum 16
+  uint32_t dl_nas_count;
+  uint32_t ul_nas_count;
+  srslte::CIPHERING_ALGORITHM_ID_ENUM cipher_algo;
+  srslte::INTEGRITY_ALGORITHM_ID_ENUM integ_algo;
+  uint8_t k_nas_enc[32];
+  uint8_t k_nas_int[32];
+  uint8_t k_enb[32];
+  LIBLTE_MME_UE_NETWORK_CAPABILITY_STRUCT ue_network_cap;
+  bool ms_network_cap_present;
+  LIBLTE_MME_MS_NETWORK_CAPABILITY_STRUCT ms_network_cap;
+} sec_ctx_t;
+
+class nas
+{
+public:
+  /*Initial UE messages*/
+  bool handle_nas_attach_request(      uint32_t enb_ue_s1ap_id,
+                                       srslte::byte_buffer_t *nas_msg,
+                                       srslte::byte_buffer_t *reply_buffer,
+                                       bool* reply_flag,
+                                       struct sctp_sndrcvinfo *enb_sri);
+
+  bool handle_nas_imsi_attach_request( uint32_t enb_ue_s1ap_id,
+                                       const LIBLTE_MME_ATTACH_REQUEST_MSG_STRUCT &attach_req,
+                                       const LIBLTE_MME_PDN_CONNECTIVITY_REQUEST_MSG_STRUCT &pdn_con_req,
+                                       srslte::byte_buffer_t *reply_buffer,
+                                       bool* reply_flag,
+                                       struct sctp_sndrcvinfo *enb_sri);
+
+  bool handle_nas_guti_attach_request( uint32_t enb_ue_s1ap_id,
+                                       const LIBLTE_MME_ATTACH_REQUEST_MSG_STRUCT &attach_req,
+                                       const LIBLTE_MME_PDN_CONNECTIVITY_REQUEST_MSG_STRUCT &pdn_con_req,
+                                       srslte::byte_buffer_t *nas_msg,
+                                       srslte::byte_buffer_t *reply_buffer,
+                                       bool* reply_flag,
+                                       struct sctp_sndrcvinfo *enb_sri);
+
+  bool handle_nas_service_request(     uint32_t m_tmsi,
+                                       uint32_t enb_ue_s1ap_id,
+                                       srslte::byte_buffer_t *nas_msg,
+                                       srslte::byte_buffer_t *reply_buffer,
+                                       bool* reply_flag,
+                                       struct sctp_sndrcvinfo *enb_sri);
+
+  bool handle_nas_detach_request(      uint32_t m_tmsi,
+                                       uint32_t enb_ue_s1ap_id,
+                                       srslte::byte_buffer_t *nas_msg,
+                                       srslte::byte_buffer_t *reply_buffer,
+                                       bool* reply_flag,
+                                       struct sctp_sndrcvinfo *enb_sri);
+
+  bool handle_nas_tracking_area_update_request( uint32_t m_tmsi,
+                                                uint32_t enb_ue_s1ap_id,
+                                                srslte::byte_buffer_t *nas_msg,
+                                                srslte::byte_buffer_t *reply_buffer,
+                                                bool* reply_flag,
+                                                struct sctp_sndrcvinfo *enb_sri);
+
+  /* Uplink NAS messages handling */
+  bool handle_nas_authentication_response(  srslte::byte_buffer_t *nas_msg, ue_ctx_t *ue_ctx, srslte::byte_buffer_t *reply_buffer, bool* reply_flag);
+  bool handle_nas_security_mode_complete(   srslte::byte_buffer_t *nas_msg, ue_ctx_t *ue_ctx, srslte::byte_buffer_t *reply_buffer, bool *reply_flag);
+  bool handle_nas_attach_complete(          srslte::byte_buffer_t *nas_msg, ue_ctx_t *ue_ctx, srslte::byte_buffer_t *reply_buffer, bool *reply_flag);
+  bool handle_esm_information_response(     srslte::byte_buffer_t *nas_msg, ue_ctx_t* ue_ctx, srslte::byte_buffer_t *reply_buffer, bool *reply_flag);
+  bool handle_identity_response(            srslte::byte_buffer_t *nas_msg, ue_ctx_t* ue_ctx, srslte::byte_buffer_t *reply_buffer, bool *reply_flag);
+  bool handle_tracking_area_update_request( srslte::byte_buffer_t *nas_msg, ue_ctx_t* ue_ctx, srslte::byte_buffer_t *reply_buffer, bool *reply_flag);
+  bool handle_authentication_failure(       srslte::byte_buffer_t *nas_msg, ue_ctx_t* ue_ctx, srslte::byte_buffer_t *reply_buffer, bool *reply_flag);
+  bool handle_nas_detach_request(           srslte::byte_buffer_t *nas_msg, ue_ctx_t* ue_ctx, srslte::byte_buffer_t *reply_buffer, bool *reply_flag);
+
+  /* Downlink NAS messages packing*/
+  bool pack_authentication_request(  srslte::byte_buffer_t *reply_msg, uint32_t enb_ue_s1ap_id, uint32_t next_mme_ue_s1ap_id, uint8_t eksi, uint8_t *autn, uint8_t *rand);
+  bool pack_authentication_reject(   srslte::byte_buffer_t *reply_msg, uint32_t enb_ue_s1ap_id, uint32_t mme_ue_s1ap_id);
+  bool pack_security_mode_command(   srslte::byte_buffer_t *reply_msg, ue_emm_ctx_t *ue_emm_ctx, ue_ecm_ctx_t *ue_ecm_ctx);
+  bool pack_esm_information_request( srslte::byte_buffer_t *reply_msg, ue_emm_ctx_t *ue_emm_ctx, ue_ecm_ctx_t *ue_ecm_ctx);
+  bool pack_identity_request(        srslte::byte_buffer_t *reply_msg, uint32_t enb_ue_s1ap_id, uint32_t mme_ue_s1ap_id);
+  bool pack_emm_information(         ue_ctx_t* ue_ctx, srslte::byte_buffer_t *reply_msg);
+  bool pack_service_reject(          srslte::byte_buffer_t *reply_msg, uint8_t emm_cause, uint32_t enb_ue_s1ap_id);
+  bool pack_attach_accept(           ue_emm_ctx_t *ue_emm_ctx, ue_ecm_ctx_t *ue_ecm_ctx, LIBLTE_S1AP_E_RABTOBESETUPITEMCTXTSUREQ_STRUCT *erab_ctxt, struct srslte::gtpc_pdn_address_allocation_ie *paa, srslte::byte_buffer_t *nas_buffer);
+
+  /* Security functions */
+  bool integrity_check(       ue_emm_ctx_t *emm_ctx, srslte::byte_buffer_t *pdu);
+  bool short_integrity_check( ue_emm_ctx_t *emm_ctx, srslte::byte_buffer_t *pdu);
+
+private:
+  emm_ctx_t m_emm_ctx;
+  ecm_ctx_t m_ecm_ctx;
+  esm_ctx_t m_esm_ctx[MAX_ERABS_PER_UE];
+  sec_ctx_t sec_ctx;
+};
+
+}//namespace
+#endif // SRSEPC_S1AP_COMMON_H
