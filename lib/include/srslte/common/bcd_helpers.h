bbbfc2138ff7efaec60c1c1385a7c81704a212de
cooperq <cooperq@cooperq.com>
2019-01-14 19:39:37 -0800

    remove unused exec function

--- lib/include/srslte/common/bcd_helpers.h
+++ lib/include/srslte/common/bcd_helpers.h
@@ -193,30 +193,6 @@ inline void s1ap_mccmnc_to_plmn(uint16_t mcc, uint16_t mnc, uint32_t *plmn)
   *plmn |= nibbles[5];
 }
 
-inline std::string exec(const char *cmd)
-{
-  char buffer[128];
-  std::string result = "";
-  FILE *pipe = popen(cmd, "r");
-  if (!pipe)
-    throw std::runtime_error("popen() failed!");
-  try
-  {
-    while (!feof(pipe))
-    {
-      if (fgets(buffer, 128, pipe) != NULL)
-        result += buffer;
-    }
-  }
-  catch (...)
-  {
-    pclose(pipe);
-    throw;
-  }
-  pclose(pipe);
-  return result;
-}
-
 } // namespace srslte
 
 #endif // SRSLTE_BCD_HELPERS_H
