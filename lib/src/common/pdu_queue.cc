c4c44c33f41c0be8c752261c958b4b03e34d02f5
Andre Puschmann <andre@softwareradiosystems.com>
2019-01-24 20:56:54 +0100

    use blocking pool_allocate in pdu_queue

--- lib/src/common/pdu_queue.cc
+++ lib/src/common/pdu_queue.cc
@@ -48,7 +48,7 @@ uint8_t* pdu_queue::request(uint32_t len)
     fprintf(stderr, "Error request buffer of invalid size %d. Max bytes %d\n", len, MAX_PDU_LEN);
     return NULL; 
   }
-  pdu_t *pdu = pool.allocate("pdu_queue::request");
+  pdu_t* pdu = pool.allocate("pdu_queue::request", true);
   if (!pdu) {
     if (log_h) {
       log_h->error("Not enough buffers for MAC PDU\n");      
