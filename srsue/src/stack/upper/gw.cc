71212dd44732edffa0f05fb4483075961491fc3b
Andre Puschmann <andre@softwareradiosystems.com>
2019-06-06 15:23:24 +0200

    remove console logging in GW

--- srsue/src/stack/upper/gw.cc
+++ srsue/src/stack/upper/gw.cc
@@ -225,9 +225,8 @@ int gw::apply_traffic_flow_template(const uint8_t&                              
 {
   switch (tft->tft_op_code) {
     case LIBLTE_MME_TFT_OPERATION_CODE_CREATE_NEW_TFT:
-      gw_log->console("Adding new TFT\n");
       for (int i = 0; i < tft->packet_filter_list_size; i++) {
-        gw_log->console("New packet filter for TFT\n");
+        gw_log->info("New packet filter for TFT\n");
         tft_packet_filter_t filter(erab_id, lcid, tft->packet_filter_list[i], gw_log);
         auto                it = tft_filter_map.insert(std::make_pair(filter.eval_precedence, filter));
         if (it.second == false) {
@@ -360,7 +359,7 @@ uint8_t gw::check_tft_filter_match(const srslte::unique_byte_buffer_t& pdu) {
       bool match = filter_pair.second.match(pdu);
       if (match) {
         lcid = filter_pair.second.lcid;
-        gw_log->console("Found filter match -- EPS bearer Id %d, LCID %d\n", filter_pair.second.eps_bearer_id, lcid);
+        gw_log->info("Found filter match -- EPS bearer Id %d, LCID %d\n", filter_pair.second.eps_bearer_id, lcid);
         break;
       }
     }
