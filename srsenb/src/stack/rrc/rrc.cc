10e4c066b57071f288b5ef6ee9eac56b44e2fba3
Aleksander Morgado <aleksander@aleksander.es>
2019-11-14 18:20:52 +0100

    enb,rrc: refactor debug_hex() call to make clang format check happy

--- srsenb/src/stack/rrc/rrc.cc
+++ srsenb/src/stack/rrc/rrc.cc
@@ -166,8 +166,13 @@ void rrc::log_rrc_message(const std::string& source, const direction_t dir, cons
   } else if (rrc_log->get_level() >= srslte::LOG_LEVEL_DEBUG) {
     asn1::json_writer json_writer;
     msg.to_json(json_writer);
-    rrc_log->debug_hex(pdu->msg, pdu->N_bytes, "%s - %s %s (%d B)\n", source.c_str(), dir == Tx ? "Tx" : "Rx",
-                       msg.msg.c1().type().to_string().c_str(), pdu->N_bytes);
+    rrc_log->debug_hex(pdu->msg,
+                       pdu->N_bytes,
+                       "%s - %s %s (%d B)\n",
+                       source.c_str(),
+                       dir == Tx ? "Tx" : "Rx",
+                       msg.msg.c1().type().to_string().c_str(),
+                       pdu->N_bytes);
     rrc_log->debug_long("Content:\n%s\n", json_writer.to_string().c_str());
   }
 }
