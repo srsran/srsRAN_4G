5e9aa38551242249ae5dec053971d23f579391d6
Andre Puschmann <andre@softwareradiosystems.com>
2020-03-25 18:30:30 +0100

    pdcp: add test for reception of dublicate PDU

--- lib/test/upper/pdcp_lte_test_rx.cc
+++ lib/test/upper/pdcp_lte_test_rx.cc
@@ -130,6 +130,27 @@ int test_rx_all(srslte::byte_buffer_pool* pool, srslte::log_ref log)
                        log) == 0);
   }
 
+  /*
+   * RX Test 3: PDCP LTE Entity with SN LEN = 12
+   * Test reception of a dublicate PDU, the duplicate should just be dropped.
+   */
+  {
+    std::vector<uint32_t> test_counts(2);                  // Test two packets
+    std::iota(test_counts.begin(), test_counts.end(), 31); // Starting at COUNT 4095
+    std::vector<pdcp_test_event_t> test_pdus = gen_expected_pdus_vector(
+        tst_sdu1, test_counts, srslte::PDCP_SN_LEN_12, srslte::PDCP_RB_IS_DRB, sec_cfg, pool, log);
+    pdcp_lte_initial_state test_init_state = {
+        .tx_count = 0, .rx_hfn = 0, .next_pdcp_rx_sn = 33, .last_submitted_pdcp_rx_sn = 32};
+    TESTASSERT(test_rx(std::move(test_pdus),
+                       test_init_state,
+                       srslte::PDCP_SN_LEN_12,
+                       srslte::PDCP_RB_IS_DRB,
+                       2,
+                       tst_sdu1,
+                       pool,
+                       log) == 0);
+  }
+
   return 0;
 }
 
