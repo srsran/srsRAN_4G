6ce33c381212402c1f588d682dafa74714a2e891
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2020-05-14 15:47:26 +0100

    Small compilation fixes for GCC 10.1

--- lib/src/phy/rf/rf_helper.h
+++ lib/src/phy/rf/rf_helper.h
@@ -82,8 +82,9 @@ static inline int parse_string(char* args, const char* config_arg_base, int chan
       printf("CHx %s=%s\n", config_arg_base, config_str);
     }
 
-    strncpy(param_dst, config_str, RF_PARAM_LEN);
-    param_dst[RF_PARAM_LEN - 1] = 0;
+    if (snprintf(param_dst, RF_PARAM_LEN, "%s", config_str) < 0) {
+      return SRSLTE_ERROR;
+    }
 
     // concatenate key=value and remove both (avoid removing the same value twice if it occurs twice in rf_args)
     char config_pair[RF_PARAM_LEN * 2] = {0};
