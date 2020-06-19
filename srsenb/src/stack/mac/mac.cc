a1a58507c1a72f2e402a03fd1f1d99d4e699fdde
Xavier Arteaga <xavier@softwareradiosystems.com>
2020-06-19 09:46:16 +0200

    SRSENB: Added eNb CC index to PRACH log trace

--- srsenb/src/stack/mac/mac.cc
+++ srsenb/src/stack/mac/mac.cc
@@ -494,8 +494,18 @@ void mac::rach_detected(uint32_t tti, uint32_t enb_cc_idx, uint32_t preamble_idx
     // Trigger scheduler RACH
     scheduler.dl_rach_info(enb_cc_idx, rar_info);
 
-    log_h->info("RACH:  tti=%d, preamble=%d, offset=%d, temp_crnti=0x%x\n", tti, preamble_idx, time_adv, rnti);
-    log_h->console("RACH:  tti=%d, preamble=%d, offset=%d, temp_crnti=0x%x\n", tti, preamble_idx, time_adv, rnti);
+    log_h->info("RACH:  tti=%d, cc=%d, preamble=%d, offset=%d, temp_crnti=0x%x\n",
+                tti,
+                enb_cc_idx,
+                preamble_idx,
+                time_adv,
+                rnti);
+    log_h->console("RACH:  tti=%d, cc=%d, preamble=%d, offset=%d, temp_crnti=0x%x\n",
+                   tti,
+                   enb_cc_idx,
+                   preamble_idx,
+                   time_adv,
+                   rnti);
   });
 
   // Allocate one new UE object in advance
