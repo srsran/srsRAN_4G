bd0c13aad096b7db4609610bc2a997bf1b3c133e
Andre Puschmann <andre@softwareradiosystems.com>
2020-07-01 17:15:57 +0200

    ue_stack: bump priority of stack thread
    the stack thread should really run with the next highest priority after
    sync + phy workers to make sure it gets enough CPU

--- srsue/hdr/stack/ue_stack_lte.h
+++ srsue/hdr/stack/ue_stack_lte.h
@@ -165,7 +165,7 @@ private:
   gw_interface_stack*      gw  = nullptr;
 
   // Thread
-  static const int        STACK_MAIN_THREAD_PRIO = 4;
+  static const int        STACK_MAIN_THREAD_PRIO = 4; // Next lower priority after PHY workers
   srslte::task_multiqueue pending_tasks;
   int sync_queue_id = -1, ue_queue_id = -1, gw_queue_id = -1, stack_queue_id = -1, background_queue_id = -1;
   srslte::task_thread_pool             background_tasks;     ///< Thread pool used for long, low-priority tasks
