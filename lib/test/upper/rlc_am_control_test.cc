2749c870e368ae21c0fab7311a9ba06b64585198
Andre Puschmann <andre@softwareradiosystems.com>
2020-07-06 22:34:26 +0200

    rlc_am: refactor control test
    * move both tests into own functions and call those in main()
    * use common test header and macros

--- lib/test/upper/rlc_am_control_test.cc
+++ lib/test/upper/rlc_am_control_test.cc
@@ -19,47 +19,62 @@
  *
  */
 
+#include "srslte/common/test_common.h"
 #include "srslte/upper/rlc_am_lte.h"
-#include <assert.h>
 #include <iostream>
 
 // Simple status PDU
-uint8_t  pdu1[]   = {0x00, 0x78};
-uint32_t PDU1_LEN = 2;
-
-// Status PDU with 4 NACKs
-uint8_t  pdu2[]   = {0x00, 0x22, 0x00, 0x40, 0x0C, 0x01, 0xC0, 0x20};
-uint32_t PDU2_LEN = 8;
-
-int main(int argc, char** argv)
+int simple_status_pdu_test1()
 {
-  srslte::rlc_status_pdu_t s1, s2;
+  uint8_t  pdu1[]   = {0x00, 0x78};
+  uint32_t PDU1_LEN = 2;
+
+  srslte::rlc_status_pdu_t s1;
   srslte::byte_buffer_t    b1, b2;
 
   memcpy(b1.msg, &pdu1[0], PDU1_LEN);
   b1.N_bytes = PDU1_LEN;
   rlc_am_read_status_pdu(&b1, &s1);
-  assert(s1.ack_sn == 30);
-  assert(s1.N_nack == 0);
+  TESTASSERT(s1.ack_sn == 30);
+  TESTASSERT(s1.N_nack == 0);
   rlc_am_write_status_pdu(&s1, &b2);
-  assert(b2.N_bytes == PDU1_LEN);
-  for (uint32_t i = 0; i < b2.N_bytes; i++)
-    assert(b2.msg[i] == b1.msg[i]);
+  TESTASSERT(b2.N_bytes == PDU1_LEN);
+  for (uint32_t i = 0; i < b2.N_bytes; i++) {
+    TESTASSERT(b2.msg[i] == b1.msg[i]);
+  }
+  return SRSLTE_SUCCESS;
+}
 
-  b1.clear();
-  b2.clear();
+// Status PDU with 4 NACKs
+int status_pdu_with_nacks_test1()
+{
+  uint8_t  pdu2[]   = {0x00, 0x22, 0x00, 0x40, 0x0C, 0x01, 0xC0, 0x20};
+  uint32_t PDU2_LEN = 8;
+
+  srslte::rlc_status_pdu_t s2;
+  srslte::byte_buffer_t    b1, b2;
 
   memcpy(b1.msg, &pdu2[0], PDU2_LEN);
   b1.N_bytes = PDU2_LEN;
   rlc_am_read_status_pdu(&b1, &s2);
-  assert(s2.ack_sn == 8);
-  assert(s2.N_nack == 4);
-  assert(s2.nacks[0].nack_sn == 0);
-  assert(s2.nacks[1].nack_sn == 1);
-  assert(s2.nacks[2].nack_sn == 3);
-  assert(s2.nacks[3].nack_sn == 4);
+  TESTASSERT(s2.ack_sn == 8);
+  TESTASSERT(s2.N_nack == 4);
+  TESTASSERT(s2.nacks[0].nack_sn == 0);
+  TESTASSERT(s2.nacks[1].nack_sn == 1);
+  TESTASSERT(s2.nacks[2].nack_sn == 3);
+  TESTASSERT(s2.nacks[3].nack_sn == 4);
   rlc_am_write_status_pdu(&s2, &b2);
-  assert(b2.N_bytes == PDU2_LEN);
-  for (uint32_t i = 0; i < b2.N_bytes; i++)
-    assert(b2.msg[i] == b1.msg[i]);
+  TESTASSERT(b2.N_bytes == PDU2_LEN);
+  for (uint32_t i = 0; i < b2.N_bytes; i++) {
+    TESTASSERT(b2.msg[i] == b1.msg[i]);
+  }
+  return SRSLTE_SUCCESS;
+}
+
+int main(int argc, char** argv)
+{
+  TESTASSERT(simple_status_pdu_test1() == SRSLTE_SUCCESS);
+  TESTASSERT(status_pdu_with_nacks_test1() == SRSLTE_SUCCESS);
+
+  return SRSLTE_SUCCESS;
 }
