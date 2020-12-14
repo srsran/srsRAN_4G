7869c66fb714d78133f1f8db52278c1e324db69c
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2020-12-14 22:54:53 +0000

    avoid overwriting phy config ul64qam config for scell

--- srsenb/src/stack/rrc/rrc_ue.cc
+++ srsenb/src/stack/rrc/rrc_ue.cc
@@ -1051,13 +1051,12 @@ void rrc::ue::apply_reconf_phy_config(const rrc_conn_recfg_r8_ies_s& reconfig_r8
             // Get corresponding eNB CC index
             scell_phy_rrc_ded.enb_cc_idx = ue_cc->cell_common->enb_cc_idx;
 
+            // Append to PHY RRC config dedicated which will be applied further down
+            phy_rrc_dedicated_list[scell.scell_idx_r10] = scell_phy_rrc_ded;
             srslte::set_phy_cfg_t_enable_64qam(
                 &phy_rrc_dedicated_list[scell.scell_idx_r10].phy_cfg,
                 ue_capabilities.support_ul_64qam and
                     parent->cfg.sibs[1].sib2().rr_cfg_common.pusch_cfg_common.pusch_cfg_basic.enable64_qam);
-
-            // Append to PHY RRC config dedicated which will be applied further down
-            phy_rrc_dedicated_list[scell.scell_idx_r10] = scell_phy_rrc_ded;
           }
         }
       }
