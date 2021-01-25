617e0ddb6739f85abe38570810a186e6c67afbf9
Andre Puschmann <andre@softwareradiosystems.com>
2021-01-25 21:49:20 +0100

    band_helper: add helper class for RF bands
    attempt to C++ify handling of radio bands and (E/NR)ARFCNs, etc.

---
+++ lib/src/common/band_helper.cc
@@ -0,0 +1,64 @@
+/**
+ *
+ * \section COPYRIGHT
+ *
+ * Copyright 2013-2020 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srslte/common/band_helper.h"
+#include <algorithm>
+
+namespace srslte {
+
+// definition of static members
+constexpr std::array<srslte_band_helper::nr_band, srslte_band_helper::nof_nr_bands_fr1>
+          srslte_band_helper::nr_band_table_fr1;
+constexpr std::array<srslte_band_helper::nr_raster_params, 3> srslte_band_helper::nr_fr_params;
+
+// Formula in 5.4.2.1
+double srslte_band_helper::nr_arfcn_to_freq(uint32_t nr_arfcn)
+{
+  nr_raster_params params = get_raster_params(nr_arfcn);
+  return (params.F_REF_Offs_MHz * 1e6 + params.delta_F_global_kHz * (nr_arfcn - params.N_REF_Offs) * 1e3);
+}
+
+// Implements 5.4.2.1 in TS 38.401
+std::vector<uint32_t> srslte_band_helper::get_bands_nr(uint32_t                             nr_arfcn,
+                                                       srslte_band_helper::delta_f_raster_t delta_f_raster)
+{
+  std::vector<uint32_t> bands;
+  for (const auto& nr_band : nr_band_table_fr1) {
+    if (nr_arfcn >= nr_band.dl_nref_first && nr_arfcn <= nr_band.dl_nref_last && nr_arfcn % nr_band.dl_nref_step == 0) {
+      // band must not already be present
+      if (std::find(bands.begin(), bands.end(), nr_band.band) == bands.end()) {
+        if (delta_f_raster == DEFAULT) {
+          // add band if delta_f_raster has default value
+          bands.push_back(nr_band.band);
+        } else {
+          // if delta_f_raster is set explicitly, only add if band matches
+          if (nr_band.delta_f_raster == delta_f_raster) {
+            bands.push_back(nr_band.band);
+          }
+        }
+      }
+    }
+  }
+  return bands;
+}
+
+srslte_band_helper::nr_raster_params srslte_band_helper::get_raster_params(uint32_t nr_arfcn)
+{
+  for (auto& fr : nr_fr_params) {
+    if (nr_arfcn >= fr.N_REF_min && nr_arfcn <= fr.N_REF_max) {
+      return fr;
+    }
+  }
+  return {}; // return empty params
+}
+
+} // namespace srslte
