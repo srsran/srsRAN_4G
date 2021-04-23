0826b86cecfea95458908272ac6f6da3ecf3bf4b
Andre Puschmann <andre@softwareradiosystems.com>
2021-04-23 21:12:23 +0200

    srsue: remove NR DL freq and ARFCN option
    not required anymore as all NR carrier tuning is done
    through RRC signaling

--- srsue/src/main.cc
+++ srsue/src/main.cc
@@ -116,8 +116,6 @@ static int parse_args(all_args_t* args, int argc, char* argv[])
     ("rat.eutra.nof_carriers", bpo::value<uint32_t>(&args->phy.nof_lte_carriers)->default_value(1), "Number of carriers")
     
     ("rat.nr.bands",        bpo::value<string>(&args->stack.rrc_nr.supported_bands_nr_str)->default_value("78"),  "Supported NR bands")
-    ("rat.nr.dl_arfcn",     bpo::value<string>(&args->phy.dl_nr_arfcn)->default_value("634240"),                  "Downlink NR-ARFCN list")
-    ("rat.nr.dl_freq",      bpo::value<double>(&args->phy.nr_freq_hz)->default_value(3513.6e6),                   "NR DL frequency")
     ("rat.nr.nof_carriers", bpo::value<uint32_t>(&args->phy.nof_nr_carriers)->default_value(0),                   "Number of NR carriers")
     ("rat.nr.max_nof_prb",  bpo::value<uint32_t>(&args->phy.nr_max_nof_prb)->default_value(106),                  "Maximum NR carrier bandwidth in PRB")
 
