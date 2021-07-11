fdebee422ef261031791eb097a089fe96dfec5a4
Codebot <codebot@softwareradiosystems.com>
2021-07-11 23:12:22 +0200

    Adding AGPL copyright to new files

--- lib/src/asn1/nas_5g_msg.cc
+++ lib/src/asn1/nas_5g_msg.cc
@@ -1,12 +1,21 @@
 /**
- *
- * \section COPYRIGHT
- *
  * Copyright 2013-2021 Software Radio Systems Limited
  *
- * By using this file, you agree to the terms and conditions set
- * forth in the LICENSE file which can be found at the top level of
- * the distribution.
+ * This file is part of srsRAN.
+ *
+ * srsRAN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * srsRAN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * A copy of the GNU Affero General Public License can be found in
+ * the LICENSE file in the top-level directory of this distribution
+ * and at http://www.gnu.org/licenses/.
  *
  */
 #include "srsran/asn1/nas_5g_msg.h"
