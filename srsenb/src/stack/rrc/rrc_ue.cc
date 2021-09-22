af73c57780a9bc158f004c5f3debaf19c7b41ae1
Andre Puschmann <andre@softwareradiosystems.com>
2021-09-22 17:00:25 +0200

    rrc_endc: prepare ENDC paramter
    exposes some of the hard-coded params as struct.
    filling/parsing from file still needs to be added.

--- srsenb/src/stack/rrc/rrc_ue.cc
+++ srsenb/src/stack/rrc/rrc_ue.cc
@@ -87,7 +87,8 @@ int rrc::ue::init()
 
   mobility_handler = make_rnti_obj<rrc_mobility>(rnti, this);
   if (parent->rrc_nr != nullptr) {
-    endc_handler = make_rnti_obj<rrc_endc>(rnti, this);
+    rrc::ue::rrc_endc::rrc_endc_cfg_t endc_cfg = {}; // TODO: set or derive parameter in eNB config
+    endc_handler                               = make_rnti_obj<rrc_endc>(rnti, this, endc_cfg);
   }
 
   return SRSRAN_SUCCESS;
