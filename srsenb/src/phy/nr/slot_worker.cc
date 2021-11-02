32c4da8db07679796298aa3ad3f0cccd96cf0c6f
dvdgrgrtt <davidg@srs.io>
2021-11-02 10:20:38 +0100

    Fix typos

--- srsenb/src/phy/nr/slot_worker.cc
+++ srsenb/src/phy/nr/slot_worker.cc
@@ -291,7 +291,7 @@ bool slot_worker::work_dl()
   }
 
   if (srsran_gnb_dl_base_zero(&gnb_dl) < SRSRAN_SUCCESS) {
-    logger.error("Error zeroeing RE grid");
+    logger.error("Error zeroing RE grid");
     return false;
   }
 
