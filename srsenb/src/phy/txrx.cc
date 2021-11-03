d5c235635fe95b753fb7d615f19dbb1cad0215c9
Ismael Gomez <ismagom@gmail.com>
2021-11-03 17:30:50 +0100

    check lteworker is not null in log line

--- srsenb/src/phy/txrx.cc
+++ srsenb/src/phy/txrx.cc
@@ -176,7 +176,7 @@ void txrx::run_thread()
           tti,
           timestamp.get(0).full_secs,
           timestamp.get(0).frac_secs,
-          lte_worker->get_id());
+          lte_worker ? lte_worker->get_id() : 0);
 
     // Trigger prach worker execution
     for (uint32_t cc = 0; cc < worker_com->get_nof_carriers_lte(); cc++) {
