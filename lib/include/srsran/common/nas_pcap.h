b389eb9d9ab54395ea62c89c1f8a8a64b5bf7e43
Merlin Chlosta <merlin.chlosta@rub.de>
2021-11-04 09:03:04 +0000

    emergency handler for NAS PCAP to close file
    Previously NAS PCAP wasn't closed/fflushed on
    unclean exit, resulting in missing or corrupted PCAP.

--- lib/include/srsran/common/nas_pcap.h
+++ lib/include/srsran/common/nas_pcap.h
@@ -31,22 +31,17 @@ namespace srsran {
 class nas_pcap
 {
 public:
-  nas_pcap()
-  {
-    enable_write = false;
-    ue_id        = 0;
-    pcap_file    = NULL;
-  }
+  nas_pcap();
   void enable();
   uint32_t open(std::string filename_, uint32_t ue_id = 0, srsran_rat_t rat_type = srsran_rat_t::lte);
   void close();
   void write_nas(uint8_t* pdu, uint32_t pdu_len_bytes);
 
 private:
-  bool        enable_write;
+  bool        enable_write = false;
   std::string filename;
-  FILE*       pcap_file;
-  uint32_t    ue_id;
+  FILE*       pcap_file = nullptr;
+  uint32_t    ue_id     = 0;
   void        pack_and_write(uint8_t* pdu, uint32_t pdu_len_bytes);
 };
 
