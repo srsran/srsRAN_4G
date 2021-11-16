440877170ffa85089fe0706191e73e30cf09bda6
Matan Perelman <matan1008@gmail.com>
2021-11-16 07:54:23 +0200

    srsUE: Add option to filter N_id_1 / SSS to configuration

--- lib/include/srsran/interfaces/ue_phy_interfaces.h
+++ lib/include/srsran/interfaces/ue_phy_interfaces.h
@@ -45,6 +45,7 @@ struct phy_args_t {
   std::map<uint32_t, uint32_t> ul_earfcn_map;    // Map linking DL EARFCN and UL EARFCN
 
   int force_N_id_2 = -1; // Cell identity within the identity group (PSS) to filter.
+  int force_N_id_1 = -1; // Cell identity group (SSS) to filter.
 
   float dl_freq = -1.0f;
   float ul_freq = -1.0f;
