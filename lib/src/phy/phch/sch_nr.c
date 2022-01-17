3e85c3bed02b3c7256541170330a20f9d50c50bd
dvdgrgrtt <davidg@srs.io>
2022-01-17 11:25:03 +0100

    Improve coding style

--- lib/src/phy/phch/sch_nr.c
+++ lib/src/phy/phch/sch_nr.c
@@ -596,6 +596,7 @@ static int sch_nr_decode(srsran_sch_nr_t*        q,
 
   // Counter of code blocks that have matched CRC
   uint32_t cb_ok = 0;
+  res->crc       = false;
 
   // For each code block...
   uint32_t j = 0;
@@ -693,7 +694,6 @@ static int sch_nr_decode(srsran_sch_nr_t*        q,
 
   // Not all CB are decoded, skip TB union and CRC check
   if (cb_ok != cfg.C) {
-    res->crc = false;
     return SRSRAN_SUCCESS;
   }
 
