179f7d8d6933e9d1f92f92d49302bcce8242bb91
Andre Puschmann <andre@srs.io>
2023-05-02 13:13:24 +0200

    asn1,test: add return value check for meas test

--- lib/test/asn1/srsran_asn1_rrc_meas_test.cc
+++ lib/test/asn1/srsran_asn1_rrc_meas_test.cc
@@ -33,7 +33,7 @@ int meas_obj_test()
   cbit_ref bref(&rrc_msg[0], sizeof(rrc_msg));
 
   ul_dcch_msg_s ul_dcch_msg;
-  ul_dcch_msg.unpack(bref);
+  TESTASSERT(ul_dcch_msg.unpack(bref) == SRSASN_SUCCESS);
 
   TESTASSERT(ul_dcch_msg.msg.type() == ul_dcch_msg_type_c::types::c1);
   TESTASSERT(ul_dcch_msg.msg.c1().type() == ul_dcch_msg_type_c::c1_c_::types::meas_report);
